3e7f9d94cc8a6770d076fd5db8adf7a610468523
Donald Geddes <hi@donaldgeddes.ca>
2022-01-26 17:37:02 -0800

    Add main parsing logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
+const parseFields = fields => {
+  if (!fields) return ''
+  return Object.keys(fields).reduce(
+    (acc, key) => (
+      (acc += key + ' '),
+      fields[key] === true
+        ? acc
+        : acc + `{ ${parseFields(fields[key]).trim()} }`
+    ),
+    ''
+  )
+}
 
+const parseArgs = args => {
+  if (!args) return ''
+  return Object.entries(args).reduce(
+    (acc, [name, value]) => (
+      (acc &&= acc + ' '),
+      (acc += ['string', 'number', 'boolean'].includes(typeof value)
+        ? `${name}: ${typeof value === 'string' ? `"${value}"` : value}`
+        : `${name}: ${value.length > 1 ? '[' : ''}${value
+            .map(val =>
+              typeof val === 'string' // catch enums
+                ? val
+                : `{ ${Object.entries(val)
+                    .map(([name, value]) => `${name}: "${value}"`)
+                    .join(', ')} }`
+            )
+            .join(', ')}${value.length > 1 ? ']' : ''}`)
+    ),
+    ''
+  )
+}
+
+const parseOps = ops =>
+  ops
+    .map(({ name, args, fields }) => {
+      const selectionSet =
+        args.length === 0 ? name : `${name} (${parseArgs(args)})`
+      return `${selectionSet} { ${parseFields(fields)} }`
+    })
+    .join(' ')
+
+export const objq = ops =>
+  `query { ${parseOps(Array.isArray(ops) === true ? ops : [ops])} }`
